776d55a90f254ffbbb90987d6dd60c412e1a114c
Antoine Kalmbach <antoine.kalmbach@qvantel.com>
2017-02-10 17:24:05 +0200

    gay

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,52 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Data.String.Strip
 import Data.Time.Clock
 import Data.Ratio
-import Data.Map.Strict
+import Data.Map.Strict as M
 import Control.Concurrent
 import Control.Monad.State
 import Control.Monad.Reader
+import Control.Monad.Trans.Maybe
+import Data.Text as T
 
-data Edge = Edge String String
+data Edge a = Edge String String (EdgeChecker a)
+
+type EdgeMap a = M.Map (T.Text, T.Text) (Edge a)
 
 type EdgeState = UTCTime
 type Checker = NominalDiffTime -> Bool
+type EdgeStateUpdater a = StateT EdgeState IO a
+
+edgeKeys :: [((T.Text, T.Text), Checker)]
+edgeKeys = [(("pieru", "perse"), mkChecker 1000), (("d", "c"), mkChecker 5000)]
+
+builder :: EdgeChecker Bool -> Checker -> IO (Bool, UTCTime)
+builder behaviour chk = do
+  k <- getCurrentTime
+  runStateT (runReaderT (runEC behaviour) chk) k
+
+bap b xs = M.map (builder b) $ M.fromList xs
+
+parseLineGetState :: M.Map (Text, Text) (IO (Bool, UTCTime)) -> T.Text -> Maybe (IO (Bool, UTCTime))
+parseLineGetState m k = M.lookup (a, b) m
+  where
+    [a, b] = T.splitOn " " k
+
+readGetState m = parseLineGetState m <$> fmap T.pack getLine
+
+bjonn = bap currentTimeChecker edgeKeys
+
+blart = do
+  s <- readGetState bjonn
+  case s of
+    Just j -> do
+      (p, k) <- j
+      print $ show p ++ show k
+      blart
+    Nothing -> putStrLn "piss off" >> blart
 
 newtype EdgeChecker a = EdgeChecker {
     runEC :: ReaderT Checker (StateT EdgeState IO) a
@@ -29,25 +63,12 @@ mkChecker :: Integer -> NominalDiffTime -> Bool
 mkChecker diff = (>=) (fromRational (diff % 1000) :: NominalDiffTime)
 
 main :: IO ()
-main = do
-  t1 <- getCurrentTime
-  threadDelay 500000
-  t2 <- getCurrentTime
-  let diff = diffUTCTime t2 t1
-  let checker = mkChecker 500
-  print diff
-  print $ checker diff
+main = blart
 
-program :: EdgeChecker Bool
-program = do
+currentTimeChecker :: EdgeChecker Bool
+currentTimeChecker = do
   time <- liftIO getCurrentTime
   trigger time 
-
-mkInitial :: EdgeChecker ()
-mkInitial = do
-  t <- liftIO getCurrentTime
-  put t
-  return ()
 
 perse :: EdgeChecker ()
 perse = do
