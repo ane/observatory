cf0f762853052a45a63d1973d8798454757eacc2
Antoine Kalmbach <antoine.kalmbach@qvantel.com>
2017-02-13 14:46:02 +0200

    Let's start over

--- app/Main.hs
+++ app/Main.hs
@@ -13,74 +13,28 @@ import Control.Monad.Reader
 import Control.Monad.Trans.Maybe
 import Data.Text as T
 
-data Edge a = Edge String String (EdgeChecker a)
+data Event a = Event
+  { timestamp :: UTCTime
+  , id :: a
+  }
 
-type EdgeMap a = M.Map (T.Text, T.Text) (Edge a)
+data Node = Node
+  { label :: Text
+  , history :: [Event Text]
+  }
 
-type EdgeState = [UTCTime]
-type Checker = NominalDiffTime -> Bool
-type EdgeStateUpdater a = StateT EdgeState IO a
+data Edge = Edge
+  { src :: Text
+  , dst :: Text
+  , state :: [Event Text]
+  }
 
-newtype EdgeChecker a = EdgeChecker {
-    runEC :: ReaderT Checker (StateT EdgeState IO) a
-    } deriving (Applicative, Functor, Monad, MonadIO, MonadReader Checker, MonadState EdgeState)
+data Check = Temporal Int Int | Cardinal Int Int Int
 
-edgeKeys :: [((T.Text, T.Text), Checker)]
-edgeKeys = [(("pieru", "perse"), mkChecker 1000), (("d", "c"), mkChecker 5000)]
-           
-builder behaviour chk = do
-  x <- getCurrentTime
-  execStateT (runReaderT (runEC behaviour) chk) [x]
+data Status = OK | WARN | NOK
 
-parseLineGetState :: Ord k => M.Map k (EdgeState) -> k -> Maybe EdgeState
-parseLineGetState m k = M.lookup k m
+type Checker = Check -> Status
 
-readGetState :: (Text, Text) -> Map (Text, Text) EdgeState -> Maybe EdgeState
-readGetState k m = parseLineGetState m k
-
-bjonn :: IO (Map (Text, Text) EdgeState)
-bjonn = mapM (builder currentTimeChecker) (M.fromList edgeKeys)
-
-runChecker :: (Text, Text) -> EdgeState -> IO (Bool, EdgeState)
-runChecker (s, d) old = runStateT (runReaderT (runEC currentTimeChecker) (mkChecker 1000)) old
-  
-blart m = do
-  [src, dst] <- (T.splitOn " " . T.pack) `fmap` getLine
-  case readGetState (src, dst) m of
-    Just oldState -> do
-      (pass, newState) <- runChecker (src, dst) oldState
-      putStrLn $ "Diff to previous: " ++ show (diffUTCTime (Prelude.head newState) (Prelude.head oldState))
-      putStrLn $ show pass ++ ", history: " ++ show newState
-      blart (M.insert (src, dst) newState m)
-    Nothing -> do
-      putStrLn "fail!"
-      blart m
-
-trigger :: UTCTime -> EdgeChecker Bool
-trigger time = do
-  checker <- ask
-  times <- get
-  modify (time:)
-  return $ checker $ diffUTCTime time (Prelude.head times)
-
-mkChecker :: Integer -> NominalDiffTime -> Bool
-mkChecker diff = (>=) (fromRational (diff % 1000) :: NominalDiffTime)
-
-main = blart bjonn
-
-currentTimeChecker :: EdgeChecker Bool
-currentTimeChecker = do
-  time <- liftIO getCurrentTime
-  trigger time 
-
-perse :: EdgeChecker ()
-perse = do
-  _ <- liftIO getChar
-  oldTimes <- get
-  newTime <- liftIO getCurrentTime
-  pass <- trigger newTime
-  let diff = diffUTCTime newTime (Prelude.head oldTimes)
-  liftIO . putStrLn $ "At " ++ show newTime ++ ": " ++ show pass ++ " delta: " ++ show diff
-  perse
-    
+main = do
+  putStrLn "hello"
   
